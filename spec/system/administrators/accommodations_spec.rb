edef523235caadcf369aa308acdd9d307bdb5690
taji <taji@sonicgarden.jp>
2025-10-28 16:59:44 +0900

    削除テスト追加

--- spec/system/administrators/accommodations_spec.rb
+++ spec/system/administrators/accommodations_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe '宿泊施設の機能', type: :system do
 
   before { sign_in administrator }
 
-  describe '表示機能' do
+  describe '表示機能', :rack_test do
     before do
       create(:accommodation, name: 'ゆけむり温泉', prefecture: Prefecture::LIST[:tokyo], address: '東京都新宿区1-1-1', phone_number: '03-1234-5678',
                              accommodation_type: 'hotel', description: '素敵な温泉旅館です。', published: true)
@@ -30,7 +30,7 @@ RSpec.describe '宿泊施設の機能', type: :system do
     end
   end
 
-  describe '登録機能' do
+  describe '登録機能', :rack_test do
     it '新規登録ができること' do
       visit administrators_accommodations_path
 
@@ -58,6 +58,41 @@ RSpec.describe '宿泊施設の機能', type: :system do
       expect(page).to have_content '長野県長野市1-2-3'
       expect(page).to have_content '026-123-4567'
       expect(page).to have_content '自然豊かな温泉旅館です。'
+    end
+  end
+
+  describe '削除機能' do
+    before do
+      create(:accommodation, name: 'さくら温泉', prefecture: Prefecture::LIST[:nagano], address: '長野県長野市1-2', phone_number: '026-987-6543',
+                             accommodation_type: 'guest_house', description: '古い良いゲストハウスです。', published: true)
+    end
+
+    it '宿泊施設の削除ができること' do
+      visit administrators_accommodations_path
+
+      expect(page).to have_content '宿泊施設一覧'
+      expect(page).to have_link 'さくら温泉'
+      expect(page).to have_content '長野県'
+      expect(page).to have_content 'ゲストハウス'
+      expect(page).to have_content '公開'
+
+      click_on 'さくら温泉'
+
+      expect(page).to have_content '宿泊施設詳細'
+      expect(page).to have_content 'さくら温泉'
+      expect(page).to have_content '長野県長野市1-2'
+      expect(page).to have_content '026-987-6543'
+      expect(page).to have_content '古い良いゲストハウスです。'
+
+      expect {
+        accept_confirm do
+          click_on '削除'
+        end
+        expect(page).to have_content '削除しました'
+      }.to change(Accommodation, :count).by(-1)
+
+      expect(page).to have_content '宿泊施設一覧'
+      expect(page).not_to have_link 'さくら温泉'
     end
   end
 end
