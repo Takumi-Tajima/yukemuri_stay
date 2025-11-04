a79ba9f9e0759d41893648ec4edb782b37b76e48
taji <taji@sonicgarden.jp>
2025-11-04 14:35:17 +0900

    ルーティングリファクタ

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :administrators, controllers: { sessions: 'administrators/sessions' }
 
   root 'accommodations#index'
 
-  resources :accommodations, only: %i[index show]
+  resources :accommodations, only: %i[index show] do
+    resources :room_types, only: %i[show], module: :accommodations
+  end
+
+  namespace :users do
+    resources :home, only: %i[index]
+  end
 
   namespace :administrators do
     root 'accommodations#index'
-    resources :accommodations, only: %i[index show new edit create update destroy]
+    resources :accommodations, only: %i[index show new edit create update destroy] do
+      resources :room_types, only: %i[show new edit create update destroy], module: :accommodations do
+        resources :room_availabilities, only: %i[new edit create update], module: :room_types
+      end
+    end
   end
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
